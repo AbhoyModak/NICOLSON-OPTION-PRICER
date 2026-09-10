a6cc70562b37f07fc8fe40c0486e6a421b53c345
ABHOY MODAK <abhoy05.com@gmail.com>
2026-09-10 20:33:15 +0530

    CONVERGENCE GRAPH

--- test and plotting/test_and_plotting.py
+++ test and plotting/test_and_plotting.py
@@ -237,13 +237,13 @@ ax_2.grid(
 )
 
 ax_2.text(
-    0.70,
+    0.80,
     0.70,
     parameter_text,
     transform=ax.transAxes,
     fontsize=10,
     verticalalignment="top",
-    horizontalalignment = "left",
+    horizontalalignment = "right",
     bbox=dict(
         boxstyle="round,pad=0.5",
         facecolor="white",
